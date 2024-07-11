824b22f081b29e808efec86db96682cf95ee4652
kenshilabs-bipin <bipin@kenshilabs.com>
2024-07-11 18:40:08 +0530

    updated the file structure

---
+++ Bipin/day1/003_two_sum/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Example usage: two_sum
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // [0, 1]
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // [1, 2]
+}
