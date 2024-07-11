659085995f580a71feae1fcda12ecf995decfcc5
kenshilabs-bipin <bipin@kenshilabs.com>
2024-07-11 18:35:23 +0530

    Updated the file structure

---
+++ Bipin/day1/001_reverse_string/reverse_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+// go test -v
+func TestReverseString(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"Non-empty string", "hello", "olleh"},
+		{"Empty string", "", ""},
+		{"String with spaces", "hello world", "dlrow olleh"},
+		{"Palindrome", "racecar", "racecar"},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			result := reverseString(tt.input)
+
+			if result != tt.expected {
+				t.Errorf("Expected %q, but got %q", tt.expected, result)
+			}
+		})
+	}
+}
