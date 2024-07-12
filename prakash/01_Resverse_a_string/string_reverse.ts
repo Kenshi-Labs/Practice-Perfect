c238373e04336cc80497faec4cbf6fceecf6966d
Prakash Srivastava <prakash@kenshilabs.com>
2024-07-12 18:58:57 +0530

    second day

---
+++ prakash/01_Resverse_a_string/string_reverse.ts
@@ -0,0 +1,5 @@
+// Method to reverse, split and join method for reverse String, it gives input to function by passing parameter str
+
+function string_reversed(str: string): string {
+  return str.split("").reverse().join("");
+}
