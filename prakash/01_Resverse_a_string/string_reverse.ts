c507053f4cd781e00d2ecb791ad02d141dcc4f53
Prakash Srivastava <prakash@kenshilabs.com>
2024-09-22 21:23:37 +0530

    4  problem solved in previous week

--- prakash/01_Resverse_a_string/string_reverse.ts
+++ prakash/01_Resverse_a_string/string_reverse.ts
@@ -1,5 +1,12 @@
 // Method to reverse, split and join method for reverse String, it gives input to function by passing parameter str
 
 function string_reversed(str: string): string {
-  return str.split("").reverse().join("");
+  // return str.split("").reverse().join("");
+
+  let reverse_str = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse_str += str[i];
+  }
+  return reverse_str;
 }
