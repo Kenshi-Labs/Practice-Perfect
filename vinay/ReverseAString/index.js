a49789768dfeaa4284857ad2d5cec9991c6a8c85
vinaykenshilabs <vinay@kenshilabs.com>
2024-10-21 10:03:12 +0000

    added for loop

--- vinay/ReverseAString/index.js
+++ vinay/ReverseAString/index.js
@@ -1,5 +1,10 @@
 function reverseString(input) {
-    return input.split('').reverse().join('');
+    let reverse_str = "";
+
+  for (let i = input.length - 1; i >= 0; i--) {
+    reverse_str += input[i];
+  }
+  return reverse_str;
 }
 
 module.exports = reverseString;
